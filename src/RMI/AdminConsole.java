50203c5ffd884f25b175457e1b09d4922d321a74
FilipeArrais <filipearrais5889@escolasdesatao.pt>
2021-03-18 23:43:35 +0000

    Tentativa da primeira funcionalidade Tava a ver como funciona, mas por algum motivo nao esta a funcionar a funçao

--- src/RMI/AdminConsole.java
+++ src/RMI/AdminConsole.java
@@ -1,20 +1,88 @@
 package RMI;
 
+import Classes.Pessoa;
+
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 // Script de Cliente diretamente ligado ao Server
 public class AdminConsole {
-
+    private static AdminInterface adminConsole;
     public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException{
 
-        AdminInterface adminConsole = (AdminInterface) Naming.lookup("Server");
+        adminConsole = (AdminInterface) Naming.lookup("Server");
         String teste;
         teste= adminConsole.teste();
         System.out.println(teste);
+        menu();
 
 
+    }
+
+    public static void menu(){
+        Scanner myObj = new Scanner(System.in);
+        String option;
+
+        while (true){
+            System.out.println("Bem vindo!");
+            System.out.println("1-Registar");
+            System.out.println("2-");
+            System.out.println("3-");
+            System.out.println("4-");
+            System.out.println("5-");
+            option= myObj.nextLine();
+
+            if(option.equals(option)){
+                //Determinar o tipo de Pessoa a criar
+                String tipo;
+                Scanner s = new Scanner(System.in);
+                System.out.println("1-Estudante");
+                System.out.println("2-Funcionario");
+                System.out.println("3-");
+                tipo = s.nextLine();
+                try{
+                    RegistoPessoa(tipo);
+                }catch (RemoteException e){
+                    System.out.println(e);
+                }
+
+            }
+        }
+    }
+
+
+    public static void  RegistoPessoa(String tipo) throws RemoteException {
+        //Recolher informaçao
+        String nome;
+        int num_eleitor;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Nome: ");
+        nome = s.nextLine();
+        System.out.print("Numero Eleitor: ");
+        num_eleitor = Integer.parseInt(s.nextLine());
+
+        // Criar objeto
+        Pessoa p = new Pessoa(nome,num_eleitor,tipo);
+
+        //chamar funçao registar do server
+        try{
+            System.out.println("aqui");
+            String r;
+            r = adminConsole.registarPessoa(p);
+            System.out.println(r);
+        }catch (RemoteException e){
+            while (true){
+                try {
+                    //Thread.sleep(1000);
+                    adminConsole = (AdminInterface) Naming.lookup("Server");
+                    break;
+                }catch(NotBoundException  | RemoteException |MalformedURLException m){
+                    System.out.println("nao conectei");
+                }
+            }
+        }
     }
 }
--- src/RMI/RMIServer.java
+++ src/RMI/RMIServer.java
@@ -1,13 +1,17 @@
 package RMI;
 
+import Classes.Pessoa;
+
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 /*very good ma friend*/
 
 public class RMIServer extends UnicastRemoteObject implements AdminInterface {
 
+    ArrayList<Pessoa> listaPessoas = new ArrayList<Pessoa>();
     protected RMIServer() throws RemoteException {
         super();
     }
@@ -16,6 +20,18 @@ public class RMIServer extends UnicastRemoteObject implements AdminInterface {
         System.out.println("olaaaa");
 
         return "olaaaaaa";
+    }
+
+    public String registarPessoa(Pessoa p){
+
+        System.out.println("Estou a registar");
+        System.out.println(p.getNome());
+        System.out.println(p.getNum_eleitor());
+        System.out.println(p.getTipo());
+
+        listaPessoas.add(p);
+
+        return "Registado";
     }
 
     public static void main(String [] args ){
